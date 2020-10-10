e131b63b2dc620c63de823d0333b26cd6c31d9f5
David Ryan <davidryan0119@gmail.com>
2020-10-10 18:32:28 -0400

    HTTPS setup

--- server/src/index.js
+++ server/src/index.js
@@ -10,15 +10,11 @@ class App
 	constructor() 
 	{
 		this.express = express();
-		this.port = process.env.PORT || 3000;
 
 		this.express.set("views", path.join(__dirname, "../public/views"));
 		this.express.set("view engine", "html");
 
 		this.initializeRoutes();
-		this.express.listen(this.port, () =>
-		console.log(`Server listening on port ${this.port}`)
-		);
 	}
 
 	/**
@@ -30,13 +26,38 @@ class App
 		this.express.use("/", express.static("/var/www/html"));
 		this.express.use("/", express.static(path.join(__dirname, "../../build")));
 
-		// Home
-		this.express
-		.route("/")
-		.get(
-			HomeController.getInstance().index.bind(HomeController.getInstance())
-		);
+		// Home Route
+		this.express.route("/").get(HomeController.getInstance().index.bind(HomeController.getInstance()));
 	}
 }
 
-const app = new App();
+/**
+ * HTTPS server.
+ */
+const server = https.createServer({
+    // key: readFileSync(path.join(__dirname, "../certs/#.key")),
+    // cert: readFileSync(path.join(__dirname, "../certs/#.crt"))
+}, new App().app);
+server.listen(443);
+
+/**
+ * HTTP server redirect to HTTPS.
+ */
+http.createServer((req, res) => 
+{
+    res.writeHead(301, { "Location": "https://" + req.headers["host"] + req.url });
+    res.end();
+}).listen(80);
+
+/**
+ * HTTPS server error callback. 
+ */
+server.on("error", (e) => { console.log("Error starting server" + e); });
+
+/**
+ * HTTPS server listening callback.
+ */
+server.on("listening", () => 
+{
+	console.log(`Server started on port ${443} on env ${process.env.NODE_ENV}`);
+});
