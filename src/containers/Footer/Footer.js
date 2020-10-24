12f81abde3bc6c01cab3b6aa9a2f87032febf7bb
David Ryan <davidryan0119@gmail.com>
2020-10-24 13:40:03 -0400

    Added LinkedIn Button

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { IconButton } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 import "./Footer.scss";
 
@@ -11,9 +12,13 @@ export default function Footer()
             <small className="copyright">
                 &copy; Copyright 2018-{new Date().getFullYear()}, David Ryan. All Rights Reserved
             </small>
+            <br/>
             <IconButton className="github-icon" href="https://github.com/DavidMRyan" target="_blank">
                 <GitHubIcon/>
             </IconButton>
+            <IconButton className="linkedin-icon" href="https://www.linkedin.com/in/david-m-ryan/" target="_blank">
+                <LinkedInIcon/>
+            </IconButton>            
         </footer>
     );
 }
